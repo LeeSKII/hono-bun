ad9c63388805fb26f7960d6cd443bf8d5ae2e031
Papilon <leeskiikseel@outlook.com>
2024-12-14 10:40:10 +0800

    update

--- src/router/book.ts
+++ src/router/book.ts
@@ -2,7 +2,7 @@ import { Hono } from "hono";
 
 const app = new Hono();
 
-app.get("/", (c) => c.json("list books"));
+app.get("/", (c) => c.json("list books11"));
 app.post("/", (c) => c.json("create a book", 201));
 app.get("/:id", (c) => c.json(`get ${c.req.param("id")}`));
 
